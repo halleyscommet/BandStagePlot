17d3aa4eacbfe0c757062f8ae9880a4be6aecfe0
Noah Harper <114954963+noahlikesvr@users.noreply.github.com>
2023-04-16 18:34:28 -0400

    THE FORM~! IT DONE DID WORKS

---
+++ JS/submitreview.js
@@ -0,0 +1,15 @@
+function submitReview () {
+  var howfind = document.getElementsByName('how-did-you-find-bandstageplot')
+  var review = document.getElementById('review-text').value
+  var howfound
+
+  for (i = 0; i < howfind.length; i++) {
+    if (howfind[i].checked) {
+      howfound = howfind[i].value
+    }
+  }
+
+  var emailLink = `mailto:bandstageplot@gmail.com?subject=BandStagePlot Review and Data&body=User found BandStagePlot by: ${howfound}.%0D%0AUser review: "${review}".`
+  window.open(emailLink, '_blank')
+  event.preventDefault()
+}
