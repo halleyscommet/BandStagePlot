f50547f03f6bb7f1864ec5a65a59f9490d2b15fc
Noah Harper <114954963+noahlikesvr@users.noreply.github.com>
2023-04-17 20:08:23 -0400

    test

--- JS/submitreview.js
+++ JS/submitreview.js
@@ -10,6 +10,9 @@ function submitReview () {
   }
 
   var emailLink = `mailto:bandstageplot@gmail.com?subject=BandStagePlot Review and Data&body=User found BandStagePlot by ${howfound}.%0D%0AUser review: "${review}".`
+  if(review === '') {
+    var emailLink = `mailto:bandstageplot@gmail.com?subject=BandStagePlot Review and Data&body=User found BandStagePlot by ${howfound}.`
+  }
   alert('This takes you to your mailing service and creates a premade email.\nFeel free to edit this!')
   window.open(emailLink,'popUpWindow','height=500,width=400,resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no,directories=no,status=yes')
   event.preventDefault()
